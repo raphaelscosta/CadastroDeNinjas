7b133e65a1cb886ac6bae69e8a4549d38707d7f7
raphaelscosta <costaraphael@id.uff.br>
2025-12-09 13:08:06 -0300

    shore: Inclui a versão do DTO e mapeia manualmente no NinjaMapper os caminhos

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String imgUrl;
+
+    private int idade;
+
+    private MissoesModel missoes;
+
+    private String rank;
+
 }
